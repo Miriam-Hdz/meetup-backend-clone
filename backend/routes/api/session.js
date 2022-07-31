4f624aa78675aa01907cdaff7e3c4ec610d68651
miriam <miriam23_hernandez@yahoo.com>
2022-07-31 16:19:21 -0500

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -20,9 +20,15 @@ router.post('/', async (req, res, next) => {
     }
 
     await setTokenCookie(res, user);
-    
+
     return res.json({ user });
 
+});
+
+// Logout
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
 });
 
 module.exports = router;
