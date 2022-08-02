d3a5cca556751e580aa97f984808f35b367f80f0
miriam <miriam23_hernandez@yahoo.com>
2022-08-02 16:20:01 -0500

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuh } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+const validateSignup = [
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a first name.'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a last name.'),
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+router.post('/', validateSignup, async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
     const user = await User.signup({ firstName, lastName, email, password });
 
